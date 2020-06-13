a94dd53bd56117f0fb6dbc271e5ba7b2f1b2b810
Franz <franz.sotoleal@ing.austral.edu.ar>
2020-06-13 13:06:52 -0300

    remove toast from reducers

--- mercado-copy-react/src/reducers/index.js
+++ mercado-copy-react/src/reducers/index.js
@@ -1,12 +1,10 @@
 import {combineReducers} from "redux";
 import cartReducer from "./cart";
 import wishlistReducer from "./wishlist";
-import toastReducer from "./toast";
 
 const allReducers = combineReducers({
     cartProducts: cartReducer,
     wishlistProducts: wishlistReducer,
-    toasts: toastReducer
 });
 
 export default allReducers
